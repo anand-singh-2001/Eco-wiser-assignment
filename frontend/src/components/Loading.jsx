431269c8f27eef43f665014737a158d815ef3e30
Anand <as504919@gmail.com>
2023-10-30 16:55:10 +0530

    Changes the loading component.

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,20 +1,13 @@
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
 const Loading = () => {
   return (
-    <div className="border border-blue-300 shadow rounded-md p-4 lg:w-[350px] md:w-[350px] sm:w-[400px] xs:w-[60%] min-w-[300px] h-[70%] mx-auto">
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-slate-700 rounded"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-              <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-            </div>
-            <div className="h-2 bg-slate-700 rounded"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Backdrop
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={true}>
+      <CircularProgress style={{ color: "white" }} />
+    </Backdrop>
   );
 };
 
